5614c8dac6fc948610cd4d1c663a35a3ad907806
kimhyobbbb <i1t28ly@gmail.com>
2022-10-25 13:43:41 +0900

    관리자 페이지 간결화

--- src/admin/js_admin/age_chart.js
+++ src/admin/js_admin/age_chart.js
@@ -1,25 +1,25 @@
 // 차트를 그럴 영역을 dom요소로 가져온다.
-var chartArea = document.getElementById('myChart').getContext('2d');
+var chartArea = document.getElementById('myAgeChart').getContext('2d');
 // 차트를 생성한다. 
 var myChart = new Chart(chartArea, {
     // ①차트의 종류(String)
-    type: 'bar',
+    type: 'line',
     // ②차트의 데이터(Object)
     data: {
         // ③x축에 들어갈 이름들(Array)
-        labels: ['10-18', '10-19', '10-20', '10-21', '10-22', '10-23', '10-24'],
+        labels: ['10대', '20대', '30대', '40대', '50대', '60대', '70대 이상'],
         // ④실제 차트에 표시할 데이터들(Array), dataset객체들을 담고 있다.
         datasets: [{
             // ⑤dataset의 이름(String)
-            label: '일자별 방문자',
+            label: '연령별 이용자',
             // ⑥dataset값(Array)
-            data: [6,12, 19, 3, 5, 8, 3],
+            data: [8,20,10,6,8,4,12],
             // ⑦dataset의 배경색(rgba값을 String으로 표현)
-            backgroundColor: '#7F11F5',
+            backgroundColor: 'transparent',
             // ⑧dataset의 선 색(rgba값을 String으로 표현)
-            // borderColor: 'rgba(255, 99, 132, 1)',
+            borderColor: 'rgb(75, 192, 192)',
             // ⑨dataset의 선 두께(Number)
-            borderWidth: 1
+            borderWidth: 2
         }]
     },
     // ⑩차트의 설정(Object)
